82610e68c0a0a4130a8ddcf36a17b65ed8cee1af
Mariana Esteban <marianavesteban@gmail.com>
2021-02-22 19:38:44 -0300

    Update Readme + fix minor bug on SocialButtons

--- components/SocialButtons/styles.tsx
+++ components/SocialButtons/styles.tsx
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
-  gap: 12px;
 
   > button {
     flex: 1;
+    &:not(:last-child) {
+      margin-right: 12px;
+    }
   }
 `;
