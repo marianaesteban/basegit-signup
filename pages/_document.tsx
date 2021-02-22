5888c580c9a6e7a7fd39aa261cd0cf17ea4d684b
Mariana Esteban <marianavesteban@gmail.com>
2021-02-22 19:10:20 -0300

    Fix missing type

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/do
 import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
